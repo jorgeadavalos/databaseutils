61fd2164919efcd18aa25e39690effe701bc36cb
jorgeadavalos <130588939+jorgeadavalos@users.noreply.github.com>
2025-01-22 14:00:12 -0500

    moving templates file

--- src/main/resources/static/includes/cmd/bootapp.cmd
+++ src/main/resources/static/includes/cmd/bootapp.cmd
@@ -1,3 +1,3 @@
 echo off
-cd \jadAllFolders\jaddev\devSTS4.20\service-utils-database\target
-java -cp "\Users\jorge\Downloads\mysql-connector-j-9.1.0.jar;service-utils-database-1.0.war" org.springframework.boot.loader.launch.WarLauncher
+cd \jadAllFolders\jaddev\devSTS4.20\databaseutils\target
+java -cp "\Users\jorge\Downloads\mysql-connector-j-9.1.0.jar;databaseutils-1.0.war" org.springframework.boot.loader.launch.WarLauncher
